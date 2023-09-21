d9730e87268977c1fb0956ca9de0046f3e27ddf7
0xcacti <0xcacti@gmail.com>
2023-09-20 23:51:00 -0400

    update neovim config to be helpful for building plugins

--- lua/tickle/init.lua
+++ lua/tickle/init.lua
@@ -1,2 +1,16 @@
 require("tickle.remap")
 require("tickle.set")
+
+P = function(v)
+    print(vim.inspect(v))
+    return v
+end
+
+RELOAD = function(...)
+    return require("plenary.reload").reload_module(...)
+end
+
+R = function(name)
+    RELOAD(name)
+    return require(name)
+end
