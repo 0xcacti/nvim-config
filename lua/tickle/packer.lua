3301fd14fec15aa207c76d4a1a138993ff83c1e7
0xcacti <0xcacti@gmail.com>
2024-09-19 01:31:37 -0400

    meow

--- lua/tickle/packer.lua
+++ lua/tickle/packer.lua
@@ -7,9 +7,12 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    -- plenary
+    use('nvim-lua/plenary.nvim')
+
     -- teleport
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
@@ -24,7 +27,6 @@ return require('packer').startup(function(use)
     }
 
     -- tree sitter
-    use('nvim-treesitter/nvim-treesitter')
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/nvim-treesitter-textobjects')
     use({
